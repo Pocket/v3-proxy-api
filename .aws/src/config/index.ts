689396295001aa59edf90ea979af95c72376201d
Srilekha Kirsh <skirsh@mozilla.com>
2023-02-13 12:56:50 -0800

    Address review comments

--- .aws/src/config/index.ts
+++ .aws/src/config/index.ts
@@ -29,7 +29,7 @@ export const config = {
   healthCheck: {
     command: [
       'CMD-SHELL',
-      'curl -f http://localhost:4029/.well-known/server-health || exit 1',
+      `curl -f http://localhost:${port}/.well-known/server-health || exit 1`,
     ],
     interval: 15,
     retries: 3,
